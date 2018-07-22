4752179efd2e5c02c0e0a753a0288c5eacbd60a1
BohdanFot <fot.bohdan.job@gmail.com>
2018-07-22 15:24:01 +0300

    All tasks without the third.

--- src/main/java/BaseTest.java
+++ src/main/java/BaseTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -13,6 +14,13 @@ public class BaseTest {
        System.setProperty("webdriver.chrome.driver", "C:\\selenium\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
+        driver.get("https://qa.reports.spd-ukraine.com/");
+        By loginField = By.name("username");
+        By passwordField = By.name("password");
+        By submitButton = By.cssSelector("[role='submit']");
+        driver.findElement(loginField).sendKeys("fot.bohdan.job@gmail.com");
+        driver.findElement(passwordField).sendKeys("123456");
+        driver.findElement(submitButton).click();
     }
 
     public WebDriverWait getElementWait(){
--- src/main/java/TimeTrackerTest.java
+++ src/main/java/TimeTrackerTest.java
@@ -4,23 +4,55 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
-import javax.swing.event.DocumentEvent;
 
 public class TimeTrackerTest extends BaseTest {
 
-    @Test
-    public void testFromLecture() {
-        driver.get("https://qa.reports.spd-ukraine.com/");
+    // MODAL WINDOWS O_O
+    public void closeUnlockedWeekModal(By locator) {
 
-        // VARIABLE LOCATORS
-        By loginField = By.name("username");
-        By passwordField = By.name("password");
-        By submitButton = By.cssSelector("[role='submit']");
-        By calendar = By.xpath("//*[contains(text(), 'Calendar')]");
+
         By closeModalButton = By.cssSelector(".close");
         By modalWindow = By.cssSelector(".modal-dialog");
-        By requestDayStatus = By.xpath("//*[contains(text(), 'Request day status')]");
+
+        try {
+            driver.findElement(locator).click();
+        } catch (WebDriverException e) {
+
+            getElementWait()
+                    .until(ExpectedConditions.elementToBeClickable(closeModalButton));
+            driver.findElement(closeModalButton).click();
+            getElementWait()
+                    .until(ExpectedConditions.invisibilityOf(driver.findElement(modalWindow)));
+            driver.findElement(locator).click();
+        }
+    }
+
+    public void closeInternalServerErrorModal(By locator) {
+
+        By errorWindow = By.cssSelector(".error-alert");
         By closeAlertButton = By.cssSelector(".btn-primary");
+
+        try {
+            driver.findElement(locator).click();
+        } catch (WebDriverException e) {
+
+            getElementWait()
+                    .until(ExpectedConditions.elementToBeClickable(closeAlertButton));
+            driver.findElement(closeAlertButton).click();
+            getElementWait()
+                    .until(ExpectedConditions.invisibilityOf(driver.findElement(errorWindow)));
+            driver.findElement(locator).click();
+        }
+    }
+
+
+    @Test
+    public void testFromLecture() {
+
+
+        // VARIABLE-LOCATORS
+        By calendar = By.xpath("//*[contains(text(), 'Calendar')]");
+        By requestDayStatus = By.xpath("//*[contains(text(), 'Request day status')]");
         By dateFrom = By.cssSelector("[placeholder='Date from']");
         By dateTo = By.cssSelector("[placeholder='Date to']");
         By dateStatusButton = By.id("s2id_autogen3");
@@ -29,45 +61,24 @@ public class TimeTrackerTest extends BaseTest {
         By requestButton = By.cssSelector(".btn-success");
         By successMessage = By.cssSelector(".success-alert");
         By requestShowing = By.xpath("//*[contains(text(), '2018-08-10 - 2018-08-20')]");
-        By removeIcons = By.xpath("//*[@title='Remove']");
-        By errorWindow = By.cssSelector(".error-alert");
-
-        driver.findElement(loginField).sendKeys("fot.bohdan.job@gmail.com");
-        driver.findElement(passwordField).sendKeys("123456");
-        driver.findElement(submitButton).click();
-
+        By removeIcon = By.xpath("//*[@title='Remove']");
 
         getElementWait().withMessage("Failed to find 'Calendar' element")
                 .ignoring(NoSuchElementException.class, StaleElementReferenceException.class)
                 .until(ExpectedConditions.presenceOfAllElementsLocatedBy(calendar));
 
-        // MODAL WINDOWS O_O
-        try {
-            driver.findElement(calendar).click();
-        } catch (WebDriverException e) {
-
-            getElementWait().until(ExpectedConditions.elementToBeClickable(closeModalButton));
-            driver.findElement(closeModalButton).click();
-            getElementWait().until(ExpectedConditions.invisibilityOf(driver.findElement(modalWindow)));
-            driver.findElement(calendar).click();
-        }
+        closeUnlockedWeekModal(calendar);
 
         getElementWait().until(ExpectedConditions
                 .presenceOfAllElementsLocatedBy(requestDayStatus));
 
-        try {
-            driver.findElement(requestDayStatus).click();
-        } catch (WebDriverException e) {
+        closeInternalServerErrorModal(requestDayStatus);
 
-            getElementWait().until(ExpectedConditions.elementToBeClickable(closeAlertButton));
-            driver.findElement(closeAlertButton).click();
-            getElementWait().until(ExpectedConditions.invisibilityOf(driver.findElement(errorWindow)));
-            driver.findElement(requestDayStatus).click();
-        }
-
-        getElementWait().until(ExpectedConditions.visibilityOfElementLocated(dateFrom));
+        getElementWait()
+                .until(ExpectedConditions.visibilityOfElementLocated(dateFrom));
         driver.findElement(dateFrom).sendKeys("2018-08-10");
-        getElementWait().until(ExpectedConditions.visibilityOfElementLocated(dateTo));
+        getElementWait()
+                .until(ExpectedConditions.visibilityOfElementLocated(dateTo));
         driver.findElement(dateTo).sendKeys(Keys.chord(Keys.CONTROL, "a"), "2018-08-20");
         driver.findElement(dateStatusButton).click();
         getElementWait()
@@ -93,18 +104,77 @@ public class TimeTrackerTest extends BaseTest {
                         driver.findElement(successMessage).isDisplayed());
         getElementWait().until(ExpectedConditions.invisibilityOfElementLocated(successMessage));
 
-        getElementWait().until(ExpectedConditions.elementToBeClickable(removeIcons));
-        WebElement current = driver.findElements(removeIcons).get(driver.findElements(removeIcons).size() - 1);
-        current.click();
-        getElementWait().until(ExpectedConditions.invisibilityOf(current));
+        getElementWait()
+                .until(ExpectedConditions.elementToBeClickable(removeIcon));
+        WebElement currentRemoveIcon = driver.findElements(removeIcon).get(driver.findElements(removeIcon).size() - 1);
+        currentRemoveIcon.click();
+        getElementWait()
+                .until(ExpectedConditions.invisibilityOf(currentRemoveIcon));
+
         try {
             getElementWait()
                     .withMessage("The request isn't deleted!!")
                     .ignoring(StaleElementReferenceException.class, NoSuchElementException.class)
-                    .until((ExpectedConditions.presenceOfElementLocated((By) current)));
+                    .until((ExpectedConditions.presenceOfElementLocated((By) currentRemoveIcon)));
         } catch (ClassCastException e) {
-
         }
+
+    }
+
+    @Test
+    public void firstTest() {
+
+        // VARIABLE-LOCATORS
+        By addTaskButton = By.cssSelector(".btn.form-control");
+        By taskNumberField = By.cssSelector(".text-uppercase.input-sm");
+        By taskDescriptionField = By.cssSelector(".ui-autocomplete-input");
+        By taskTimeField = By.cssSelector(".col-xs-11.form-control.input-sm");
+        By saveAllButton = By.cssSelector(".btn-danger");
+        By removeIcon = By.cssSelector(".glyphicon-remove");
+        By confirmModalButton = By.cssSelector(".confirm");
+        By confirmDeleteModalWindow = By.cssSelector(".modal-backdrop");
+        By cancelDeleteButton = By.cssSelector(".cancel");
+
+        closeUnlockedWeekModal(addTaskButton);
+
+        driver.findElements(taskNumberField).get(driver.findElements(taskNumberField).size() - 1).sendKeys("TEST");
+        driver.findElements(taskDescriptionField).get(driver.findElements(taskDescriptionField).size() - 1).sendKeys("TEST DESCRIPTION");
+        driver.findElements(taskTimeField).get(driver.findElements(taskTimeField).size() - 1).sendKeys("0.1");
+        getElementWait()
+                .ignoring(NoSuchElementException.class, StaleElementReferenceException.class)
+                .until(ExpectedConditions.presenceOfElementLocated(saveAllButton));
+        driver.findElement(saveAllButton).click();
+
+        WebElement currentRemoveIcon = driver.findElements(removeIcon).get(driver.findElements(removeIcon).size() - 1);
+        getElementWait()
+                .until(ExpectedConditions.invisibilityOfElementLocated(confirmDeleteModalWindow));
+        currentRemoveIcon.click();
+        getElementWait()
+                .until(ExpectedConditions.elementToBeClickable(driver.findElements(cancelDeleteButton).get(driver.findElements(cancelDeleteButton).size() - 1)));
+        driver.findElements(confirmModalButton).get(driver.findElements(confirmModalButton).size() - 1).click();
+
+
+        try {
+            getElementWait()
+                    .withMessage("The task isn't deleted!!")
+                    .ignoring(StaleElementReferenceException.class, NoSuchElementException.class)
+                    .until((ExpectedConditions.presenceOfElementLocated((By) currentRemoveIcon)));
+        } catch (ClassCastException e) {
+        }
+    }
+
+
+    //BLOCKED BY THE "Project not found." ERROR WINDOWS ON THE "USERS" TAB
+    @Test
+    public void secondTest() {
+
+        By users = By.xpath("//*[contains(text(), 'Users')]");
+
+        getElementWait().withMessage("Failed to find 'Users' element")
+                .ignoring(NoSuchElementException.class, StaleElementReferenceException.class)
+                .until(ExpectedConditions.presenceOfAllElementsLocatedBy(users));
+
+        closeUnlockedWeekModal(users);
 
     }
 }
