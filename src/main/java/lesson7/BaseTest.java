11a4de3b27fd3f2445788df8cd5d2efb2e1af76e
valeriiborovyk <borovykvalerii@gmail.com>
2018-07-23 22:50:11 +0300

    homework5 is done

--- src/main/java/lesson7/BaseTest.java
+++ src/main/java/lesson7/BaseTest.java
@@ -2,27 +2,40 @@ package lesson7;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BaseTest {
 
-    protected WebDriver driver;
+    protected static WebDriver driver;
 
     @BeforeEach
     public void prepare() {
-       System.setProperty("webdriver.chrome.driver", "C:\\selenium\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "/Applications/chromedriver");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
     }
 
-    public WebDriverWait getElementWait(){
+    public WebDriverWait getElementWait() {
         return new WebDriverWait(driver, 10);
     }
 
     @AfterEach
     public void tearDown() {
         driver.quit();
+    }
+
+
+    public static void logIn() {
+        driver.get("https://qa.reports.spd-ukraine.com/");
+
+        By name = By.name("username");
+
+        driver.findElement(name).sendKeys("e.voloshyn@spd-ukraine.com");
+        driver.findElement(By.name("password")).sendKeys("JMSgzZVz");
+
+        driver.findElement(By.cssSelector("[role='submit']")).click();
     }
 }
