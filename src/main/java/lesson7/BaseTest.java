1970733c94310e8dae2027d4380c74aedd88101e
Tatyana Dovgan <>
2018-07-24 23:12:59 +0300

    Home Work 5

--- src/main/java/lesson7/BaseTest.java
+++ src/main/java/lesson7/BaseTest.java
@@ -2,6 +2,7 @@ package lesson7;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -12,9 +13,15 @@ public class BaseTest {
 
     @BeforeEach
     public void prepare() {
-       System.setProperty("webdriver.chrome.driver", "C:\\selenium\\chromedriver.exe");
+       System.setProperty("webdriver.chrome.driver", "/Users/tanya/Documents/chromedriver/chromedriver");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
+        driver.get("https://qa.reports.spd-ukraine.com/");
+        By name = By.name("username");
+        driver.findElement(name).sendKeys("tatyana.dovhan@gmail.com");
+        driver.findElement(By.name("password")).sendKeys("4mohWUCg");
+        driver.findElement(By.cssSelector("[role='submit']")).click();
+
     }
 
     public WebDriverWait getElementWait(){
